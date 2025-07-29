e7e1cd53eb206811ff1dc835bbbbe6900240379e
Varshith Chilagani <varshithchilagani03@gmail.com>
2025-07-29 21:27:06 +0530

    Update snowflake_script.sql

--- scripts/snowflake_script.sql
+++ scripts/snowflake_script.sql
@@ -12,14 +12,14 @@ CREATE OR REPLACE STORAGE INTEGRATION news_data_s3_integration
 TYPE = EXTERNAL_STAGE
 STORAGE_PROVIDER = 'S3'
 ENABLED = TRUE
-STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::880265509658:role/snowflakerole'
-STORAGE_ALLOWED_LOCATIONS = ('s3://my-news-bucket-varshith/news_data/');
+STORAGE_AWS_ROLE_ARN = 'enter arn'
+STORAGE_ALLOWED_LOCATIONS = ('s3://s3-bucket-name/folder-name/');
 
 
 DESC INTEGRATION news_data_s3_integration;
 
 CREATE OR REPLACE STAGE s3_raw_data_stage
-URL = 's3://my-news-bucket-varshith/news_data/'
+URL = 's3://s3-bucket-name/folder-name/'
 STORAGE_INTEGRATION = news_data_s3_integration
 FILE_FORMAT = (TYPE = 'PARQUET');
 
